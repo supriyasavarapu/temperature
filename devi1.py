9fed3f3eaef912508986b747cd0227a0a652edd5
supriyasavarapu <146428159+supriyasavarapu@users.noreply.github.com>
2025-07-08 20:38:07 +0700

    Add files via upload

---
+++ devi1.py
@@ -0,0 +1,10 @@
+###################FINDING THE TEMPERATURE WARM,COOL OR HOT #################
+
+
+# temp=int(input("enter temperature value"))
+# if temp<22:
+#     print("cool")
+# elif temp>=22 and temp<32:                    #o/p=enter temperature value33 ==>hot
+#     print("warm")
+# else:
+#     print("hot")    
